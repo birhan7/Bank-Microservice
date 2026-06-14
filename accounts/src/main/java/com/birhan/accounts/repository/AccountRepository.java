f4c5b7a624d4b98619738ead8dde9544e2e5ab86
birhan <birhanaklilu102@gmail.com>
2026-06-15 00:51:13 +0300

    added basic entities and configure repository

---
+++ accounts/src/main/java/com/birhan/accounts/repository/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.birhan.accounts.repository;
+
+import com.birhan.accounts.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+}
